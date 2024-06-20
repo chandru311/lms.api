dad48ca3f62d97e99c155029bc7b33f26a1a0a82
chandru <chandru@ai4soln.com>
2024-06-20 12:07:16 +0530

    added swagger config

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,8 +7,13 @@ namespace lms.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UserController : ControllerBase
     {
-        
+        public UserController()
+        {
+            
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -36,9 +36,10 @@ builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericReposi
 
 #endregion
 
-#region
+#region JWT
 
 builder.Services.AddAuthenticationConfigJwt(builder.Configuration);
+builder.Services.AddSwaggerConfiguration(builder.Configuration);
 
 #endregion
 
@@ -46,21 +47,21 @@ builder.Services.AddAuthenticationConfigJwt(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("CustomPolicy");
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
